90c6c630cfd1fad25bfacc5f321857568fc1ee61
Frank Labrador <labradorf95@gmail.com>
2023-11-25 12:07:07 -0300

    se añade otro gasto

--- js/main.js
+++ js/main.js
@@ -5,14 +5,17 @@ function simuladorAhorros(salario, arriendo, seguroVehiculo) {
 
     for (let i = 0; i < numMeses; i+=1) {
         //rango entre 200$ y 50$
-        let comida = Math.ceil(Math.random() * (200 - 50 + 1)) + 50;
+        let comida = Math.ceil(Math.random() * (200 - 100 + 1)) + 100;
         //rango entre 40$ y 15$
         let luz = Math.ceil(Math.random() * (40 - 15 + 1)) + 15;
         //rango 20$ y 5$
         let agua = Math.ceil(Math.random() * (20 - 5 + 1)) + 5;
         //rango entre 80$ y 50$
-        let impuestos = Math.ceil(Math.random() * (80 - 50 + 1)) + 50;
-        let ahorro = salario - arriendo - comida - luz - agua - seguroVehiculo - impuestos;
+        let impuestos = Math.ceil(Math.random() * (100 - 50 + 1)) + 50;
+        //Capricho "necesario"
+        let capricho = Math.ceil(Math.random()) * 200; 
+
+        let ahorro = salario - arriendo - comida - luz - agua - seguroVehiculo - impuestos - capricho;
 
 
         if (ahorro > 0) {
