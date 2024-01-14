47be7e2690db4cd1c56185daff5f1ae92d067462
Frank Labrador <labradorf95@gmail.com>
2024-01-14 14:45:37 -0300

    implementacion del resultado a la tabla

--- js/main.js
+++ js/main.js
@@ -1,71 +1,9 @@
-
-function simuladorAhorros(salario, arriendo, seguroVehiculo) {
-    let totalAhorro = 0;
-    const nombreMeses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-    const numMeses = Number(prompt("ingrese el numero de meses a simular"))
-
-
-    const listaDeGastos = []
-    for (let i = 0; i < numMeses; i += 1) {
-        let gasto = {
-            //rango entre 200$ y 50$
-            comida: Math.ceil(Math.random() * (200 - 100 + 1)) + 100,
-            //rango entre 40$ y 15$
-            luz: Math.ceil(Math.random() * (40 - 15 + 1)) + 15,
-            //rango 20$ y 5$
-            agua: Math.ceil(Math.random() * (20 - 5 + 1)) + 5,
-            //rango entre 80$ y 50$
-            impuestos: Math.ceil(Math.random() * (100 - 50 + 1)) + 50,
-            //Capricho "necesario"
-            capricho: Math.ceil(Math.random()) * 200,
-
-            mes: nombreMeses[i%12]
-        }
-//agregamos al array el objeto gasto
-        listaDeGastos.push(gasto)
-
-        let ahorro = salario - arriendo - gasto.comida - gasto.luz - gasto.agua - seguroVehiculo - gasto.impuestos - gasto.capricho;
-
-
-
-
-        if (ahorro > 0) {
-            console.log(nombreMeses[i%12] + ": Pudiste ahorrar " + ahorro + " Dolares ");
-            totalAhorro += ahorro;
-        } else {
-            console.log(nombreMeses[i%12] + ": No ahorraste nada, salame");
-        }
-
-    }
-
-    console.log("Ahorro total en " + numMeses + " meses: " + totalAhorro + " Dolares");
-    alert("Ahorro total en " + numMeses + " meses: " + totalAhorro + " Dolares");
-
-    console.table(listaDeGastos)
-//Resultado del mes de febrero en este metodo
-    let gastosFebrero = filtroPorMes(listaDeGastos,"Febrero")
-    console.table(gastosFebrero);
-}
-
-//metodo para dar resultados por mes en el simulador
-function filtroPorMes(listaDeGastos, mes){
-    
-    return listaDeGastos.filter(gasto => gasto.mes==mes)
-}
-
-//simuladorAhorros(1200, 600, 100);
-
-
-
-
-
 //Creamos la funcion para crear el simulador de ahorro que interactua con el DOM
-function simuladorAhorro(){
-    const nombreMeses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+function simuladorAhorro() {
 
     let salario = parseInt(document.querySelector("#salario").value);
     let arriendo = parseInt(document.querySelector("#arriendo").value);
-    let transporte =parseInt( document.querySelector("#transporte").value);
+    let transporte = parseInt(document.querySelector("#transporte").value);
     let comida = parseInt(document.querySelector("#comida").value);
     let luz = parseInt(document.querySelector("#luz").value);
     let agua = parseInt(document.querySelector("#agua").value);
@@ -73,55 +11,47 @@ function simuladorAhorro(){
     let gastosVarios = parseInt(document.querySelector("#gastosVarios").value);
 
     let gastos = arriendo + transporte + comida + luz + agua + impuestos + gastosVarios;
-    let gastoTotal = salario - gastos;
-    document.querySelector("#resultado").value = gastoTotal;
+    let ahorroTotal = salario - gastos;
+
+
 
     //colocamos una condicion para cuando den numeros negativos
-    let resultadoInput = document.querySelector("#resultado");
-    if (gastoTotal >= 0) {
-        resultadoInput.value = gastoTotal;
+    if (ahorroTotal > 0) {
+        let resultadoInput = document.querySelector("#resultado");
+        console.log("hubo ahorro");
+        resultadoInput.value = ahorroTotal;
+
     } else {
-        //cambiamos temporalmente de input a text
-        resultadoInput.type = "text";
-        resultadoInput.value = "No Ahorraste nada :C";
-        //luego retomamos el input a number
-        setTimeout(function() {
-            resultadoInput.type = "number";
-            resultadoInput.value = "";
-        }, 5000);
+        console.log("no hubo ahorro");
+        document.querySelector("#noAhorro").classList.remove("noAhorrasteOculto")
+        setTimeout(() => {
+            document.querySelector("#noAhorro").classList.add("noAhorrasteOculto")
+        }, 5000)
     }
 
-    //se utiliza un nombre aleatorio de prueba
-    let usuario = [{ 
-        "usuario": {
-            "nombre": "Juan",
-            "apellido": "Perez"
-        } 
-    }]
-    
-    //array de objeto para el calculo de meses
-    nombreMeses.forEach((mes, index) => {
-        let calculo =  {
-            "salario" : salario,
-            "arriendo" : arriendo,
-            "transporte" : transporte* (index+1),
-            "comida" : comida* (index+1),
-            "luz" : luz* (index+1),
-            "agua" : agua* (index+1),
-            "impuestos" : impuestos* (index+1),
-            "gastosVarios" : gastosVarios* (index+1),
-        }
-        usuario.push(calculo);
-        
-    });
+    guardarResultado(ahorroTotal);
 
-    let jsonString = JSON.stringify(usuario);
-    
-    localStorage.setItem("resultado", jsonString);
 }
-    
+//creamos funcion para que reciba de parametro del resultado del ahorro y guarde en el localStorage
+function guardarResultado(resultadoAhorro) {
+    const jsonString = localStorage.getItem("datosUsuario");
+    let usuario;
 
+    if (jsonString) {
+        usuario = JSON.parse(jsonString);
+        usuario.resultado.push(resultadoAhorro);
+    } else {
+        usuario = {
+            "nombre": "Juan",
+            "apellido": "Perez",
+            "resultado": [resultadoAhorro]
+        }
+    }
 
+    console.log(usuario);
+    let usuarioString = JSON.stringify(usuario)
+    localStorage.setItem("datosUsuario", usuarioString);
+}
 
 
 
---
+++ js/tabla.js
@@ -0,0 +1,52 @@
+// tabla.js
+//funcion para buscar y obtener el resultado en el localStorage del usuario
+function obtenerResultadosLocalStorage() {
+    const jsonString = localStorage.getItem("datosUsuario");
+    return jsonString ? JSON.parse(jsonString) : {
+        "nombre": "",
+        "apellido": "",
+        "resultado": []
+    };
+}
+let mesActual = 0;
+
+function agregarResultadoATabla() {
+    let tabla = document.querySelector("#tablaDeAhorro");
+    let usuario = obtenerResultadosLocalStorage();
+    document.querySelector("#usuarioTabla").value = usuario.nombre + " " + usuario.apellido;
+
+    usuario.resultado.forEach((ahorro, index) => {
+        let fila = tabla.insertRow();
+        let celdaMes = fila.insertCell(0);
+        let celdaAhorro = fila.insertCell(1);
+
+        // Asignar valores a las celdas
+        celdaMes.textContent = obtenerMeses(index);
+        celdaAhorro.textContent = ahorro;
+
+        // Incrementar el mes actual para la siguiente iteración
+        mesActual = (mesActual + 1) % 12;
+    });
+}
+
+function obtenerMeses(indice) {
+    const meses = [
+        "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+        "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+    ];
+
+    return meses[indice];
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    agregarResultadoATabla();
+});
+
+
+
+
+
+
+
+
